7baac258afae9ee2ae06f357b4f2d64feb26eedf
joel ibaceta <mail@joelibaceta.com>
2021-09-11 18:51:36 -0500

    suma

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+app.get('/suma/:a/:b', (req, res) => {
+    let a = parseInt(req.param("a"))
+    let b = parseInt(req.param("b"))
+    res.send("Resultado: " + (a + b))
+})
+
 app.listen(process.env.PORT || 3000  )
